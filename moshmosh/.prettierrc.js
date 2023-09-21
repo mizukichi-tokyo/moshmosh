5658544220501829ecf03e4ccde898a1e80bf7c2
mizukichi-soul <mizukijan@gmail.com>
2023-09-21 19:52:54 +0900

    pretitterrcを修正

--- moshmosh/.prettierrc.js
+++ moshmosh/.prettierrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  printWidth: 100,
-  singleQuote: true,
-  trailingComma: 'all',
+    printWidth: 100,
+    singleQuote: true,
+    trailingComma: "es5",
+    useTabs: true,
+    tabWidth: 4,
 };
